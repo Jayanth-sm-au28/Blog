deb2205beee13dbdb61e2e8fe22f775de0970db9
Jayanthoutnondgenie <jayanth@outbondgenie.com>
2024-05-31 01:06:42 +0530

    tailwind config added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,20 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          
+      },
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        md: "425px",
+        lg: "768px",
+        xl: "900px",
+        "2xl": "1400px",
       },
     },
   },
+ 
+
   plugins: [],
 };
 export default config;
